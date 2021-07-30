db5dc5af62f3e332332e180c1f011148cc9cc27e
Jared Cohen <jaredcohe@gmail.com>
2021-07-30 15:29:58 -0400

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,11 +1,25 @@
 function sortByDate() {
   Logger.log('General.gs > sortByDate');
+  
+  // Get the sheet to sort
   var activeSheet = SpreadsheetApp.getActiveSheet();
+  
+  // Get the last row with data to be the last row of the range to sort
   var lastRowOfData = activeSheet.getLastRow();
+  
+  // Get the last column with data to be the last column of the range to sort
   var lastColumnOfData = activeSheet.getLastColumn();
+  
+  // Give the first row of data to start sorting in
   var firstRowOfData = 7;
+  
+  // Give the column number to use as the sorting basis
   var sortColumnNumber = 6;
+  
+  // Set the range to sort
   var range = activeSheet.getRange(firstRowOfData, 1, lastRowOfData - firstRowOfData + 1, lastColumnOfData);
+  
+  // Sort!
   range.sort({column: sortColumnNumber, ascending: true});
 };
 
