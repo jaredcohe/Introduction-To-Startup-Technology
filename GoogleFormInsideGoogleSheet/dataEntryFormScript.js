7e94891bd2da23df3aac3a3d005040d43f24f864
Jared Cohen <jaredcohe@gmail.com>
2021-08-09 18:03:53 -0400

    Create dataEntryFormScript.js

---
+++ GoogleFormInsideGoogleSheet/dataEntryFormScript.js
@@ -0,0 +1,22 @@
+function showDataEntryForm(e) {
+  Logger.log('showDataEntryForm');
+  
+  var html = HtmlService.createHtmlOutputFromFile('Data Entry Form Template')
+      .setTitle('Data Entry Form')
+      .setWidth(300);
+      
+  SpreadsheetApp.getUi()
+      .showSidebar(html);
+};
+
+function onSubmitForm(data) {
+  var ui = SpreadsheetApp.getUi();
+  ui.alert('Form submitted');
+  Logger.log(data);
+  
+  var file = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = file.getSheetByName("Data Entry - Internal Form");
+  var lastRow = sheet.getLastRow();
+  var today = new Date();
+  sheet.getRange(lastRow + 1, 1, 1, 6).setValues([[data["itemId"], data["category"], data["package"], data["price"], data["count"], today]]);
+};
