af25deec159ab1b1e73556f7bbc57132ebe72024
Jared Cohen <jaredcohe@gmail.com>
2021-08-13 08:34:25 -0400

    Update dataEntryFormScript.js

--- GoogleFormInsideGoogleSheet/dataEntryFormScript.js
+++ GoogleFormInsideGoogleSheet/dataEntryFormScript.js
@@ -15,7 +15,7 @@ function onSubmitForm(data) {
   Logger.log(data);
   
   var file = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = file.getSheetByName("Data Entry - Internal Form");
+  var sheet = file.getSheetByName("Data Entry - Embedded Form");
   var lastRow = sheet.getLastRow();
   var today = new Date();
   sheet.getRange(lastRow + 1, 1, 1, 6).setValues([[data["itemId"], data["category"], data["package"], data["price"], data["count"], today]]);
